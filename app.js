8e67bff4265c1835829f123d1a7d62bbadb01852
Vimal <74707948+Vimal-Prog@users.noreply.github.com>
2020-12-26 15:36:11 +0530

    Version-1

---
+++ app.js
@@ -0,0 +1,30 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamberger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+const loader=document.getElementById('load')
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	console.log(scroll_position)
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+function Loader(){
+	loader.style.display = 'none';
+}
